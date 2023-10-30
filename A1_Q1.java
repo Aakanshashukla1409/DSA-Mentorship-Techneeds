b5f6b4a96ec21d4a40579dd0efdd3bbbbeb24bf4
Aakanshashukla1409 <149365999+Aakanshashukla1409@users.noreply.github.com>
2023-10-30 07:41:30 -0700

    II answered Q1 of assignment 1....

---
+++ A1_Q1.java
@@ -0,0 +1,13 @@
+public class QUESTIONS{
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+
+
+    }
+}
